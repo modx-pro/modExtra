95e9c8b6585da24c43892487da93a6c8e12e41ac
sergant210 <sergant210@bk.ru>
2015-09-02 09:49:52 +0300

    Support the relevance of the tables structure

--- _build/resolvers/resolve.tables.php
+++ _build/resolvers/resolve.tables.php
@@ -1,17 +1,17 @@
 <?php
 
 if ($object->xpdo) {
-	/** @var modX $modx */
-	$modx =& $object->xpdo;
+    /** @var modX $modx */
+    $modx =& $object->xpdo;
 
-	switch ($options[xPDOTransport::PACKAGE_ACTION]) {
-		case xPDOTransport::ACTION_INSTALL:
+    switch ($options[xPDOTransport::PACKAGE_ACTION]) {
+        case xPDOTransport::ACTION_INSTALL:
         case xPDOTransport::ACTION_UPGRADE:
-			$modelPath = $modx->getOption('modextra_core_path', null, $modx->getOption('core_path') . 'components/modextra/') . 'model/';
-			$modx->addPackage('modextra', $modelPath);
+            $modelPath = $modx->getOption('modextra_core_path', null, $modx->getOption('core_path') . 'components/modextra/') . 'model/';
+            $modx->addPackage('modextra', $modelPath);
 
-			$manager = $modx->getManager();
-			$objects = array();
+            $manager = $modx->getManager();
+            $objects = array();
             $schemaFile = MODX_CORE_PATH . 'components/modextra/model/schema/modextra.mysql.schema.xml';
             if (is_file($schemaFile)) {
                 $schema = new SimpleXMLElement($schemaFile, 0, true);
@@ -24,7 +24,7 @@ if ($object->xpdo) {
             } else {
                 $modx->log(modX::LOG_LEVEL_ERROR, 'Could not get classes from schema file.');
             }
-			foreach ($objects as $tmp) {
+            foreach ($objects as $tmp) {
                 // Operate with tables
                 $manager->createObjectContainer($tmp);
 
@@ -35,8 +35,8 @@ if ($object->xpdo) {
                     $tableFields[] = $cl['Field'];
                 }
 
-                foreach ($manager->xpdo->getFields($tmp) as $field=>$v) {
-                    if (in_array($field,$tableFields)) {
+                foreach ($manager->xpdo->getFields($tmp) as $field => $v) {
+                    if (in_array($field, $tableFields)) {
                         unset($tableFields[$field]);
                         $manager->alterField($tmp, $field);
                     } else {
@@ -48,8 +48,8 @@ if ($object->xpdo) {
                 }
                 // Operate with indexes
                 if ($options[xPDOTransport::PACKAGE_ACTION] == xPDOTransport::ACTION_INSTALL) {
-                    foreach ($modx->getIndexMeta($tmp) as $name=>$meta) {
-                        $manager->addIndex($tmp,$name);
+                    foreach ($modx->getIndexMeta($tmp) as $name => $meta) {
+                        $manager->addIndex($tmp, $name);
                     }
                 } else {
                     $indexes = array();
@@ -58,21 +58,21 @@ if ($object->xpdo) {
                     while ($cl = $c->fetch(PDO::FETCH_ASSOC)) {
                         $indexes[] = $cl['Key_name'];
                     }
-                    foreach ($modx->getIndexMeta($tmp) as $name=>$meta) {
-                        if (in_array($name,$indexes)) {
+                    foreach ($modx->getIndexMeta($tmp) as $name => $meta) {
+                        if (in_array($name, $indexes)) {
                             unset($indexes[$name]);
                         } else {
-                            $manager->addIndex($tmp,$name);
+                            $manager->addIndex($tmp, $name);
                         }
                     }
                     foreach ($indexes as $index) {
                         $manager->removeIndex($tmp, $index);
                     }
                 }
-			}
-			break;
+            }
+            break;
 
-		case xPDOTransport::ACTION_UNINSTALL:
+        case xPDOTransport::ACTION_UNINSTALL:
             // Remove tables if it's need
             /*
             $modelPath = $modx->getOption('modextra_core_path', null, $modx->getOption('core_path') . 'components/modextra/') . 'model/';
@@ -96,7 +96,7 @@ if ($object->xpdo) {
                 $manager->removeObjectContainer($tmp);
             }
             */
-			break;
-	}
+            break;
+    }
 }
 return true;
