ab831e774857a1b9f8e2eeb3b0b5c01f60dbc284
Sergey Shlokov <sergant210@bk.ru>
2016-04-15 19:35:20 +0300

    Update connector.php

--- assets/components/modextra/connector.php
+++ assets/components/modextra/connector.php
@@ -1,6 +1,11 @@
 <?php
-/** @noinspection PhpIncludeInspection */
-require_once dirname(dirname(dirname(dirname(__FILE__)))) . '/config.core.php';
+if (file_exists(dirname(dirname(dirname(dirname(__FILE__)))) . '/config.core.php')) {
+    /** @noinspection PhpIncludeInspection */
+    require_once dirname(dirname(dirname(dirname(__FILE__)))) . '/config.core.php';
+}
+else {
+    require_once dirname(dirname(dirname(dirname(dirname(__FILE__))))) . '/config.core.php';
+}
 /** @noinspection PhpIncludeInspection */
 require_once MODX_CORE_PATH . 'config/' . MODX_CONFIG_KEY . '.inc.php';
 /** @noinspection PhpIncludeInspection */
